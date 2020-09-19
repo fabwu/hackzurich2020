b686b96fe19de1a0bd091da5040ee1149399ff89
Fabian Wüthrich <me@fabwu.ch>
2020-09-19 03:44:03 +0200

    Add eaternity wrapper

---
+++ src/backend/eaternity.py
@@ -0,0 +1,147 @@
+import requests
+import time
+
+from requests.auth import HTTPBasicAuth
+
+YOUR_KEY = "SR007FDeykS4qlpQXRtvfa2BEUsJjYKz"
+AUTH = HTTPBasicAuth(YOUR_KEY, "")
+BASE_URL = "https://co2.eaternity.ch"
+DUMMY_KITCHEN_NAME = 'my_first_kitchen'
+
+if YOUR_KEY == "CHANGEME":
+    raise RuntimeError("Please change your api key!!")
+
+
+def get_kitchens():
+    url = f"{BASE_URL}/api/kitchens/"
+
+    response = requests.get(url, auth=AUTH)
+
+    if response.status_code not in [200, 201, 202]:
+        print(
+            f"ERROR: Failed GETting kitchen with status {response.status_code}: '{response.text}'")
+    else:
+        print(f"SUCCESS: GET kitchen")
+        return response.json()
+
+
+def get_all_recipes():
+    url = f"{BASE_URL}/api/kitchens/{DUMMY_KITCHEN_NAME}/recipes"
+
+    response = requests.get(url, auth=AUTH)
+
+    if response.status_code not in [200, 201, 202]:
+        print(
+            f"ERROR: Failed GETting kitchen with status {response.status_code}: '{response.text}'")
+    else:
+        print(f"SUCCESS: GET kitchen")
+        return response.json()
+
+
+def delete_recipe(recipe_id):
+    url = f"{BASE_URL}/api/kitchens/{DUMMY_KITCHEN_NAME}/recipes/{recipe_id}"
+
+    response = requests.delete(url, auth=AUTH)
+
+    if response.status_code not in [204]:
+        print(
+            f"ERROR: Failed deleting recipe {recipe_id} with status {response.status_code}: '{response.text}'")
+
+
+def delete_all_recipes():
+    """ Removes all recipes (e.g. for cleanup) """
+
+    url = f"{BASE_URL}/api/kitchens/{DUMMY_KITCHEN_NAME}/recipes"
+
+    response = requests.get(url, auth=AUTH)
+
+    if response.status_code not in [200, 201, 202]:
+        print(
+            f"ERROR: Failed GETting all recipes with status {response.status_code}: '{response.text}'")
+    else:
+        recipes = response.json()['recipes']
+
+        for recipe_id in recipes:
+            delete_recipe(recipe_id)
+
+
+def get_score(ingredients):
+    """
+    Takes a dict with ingredients and returns environment scores
+
+    [
+    {
+        "name": "Chilli",
+        "lang": "de",
+        "amount": 100,
+        "unit": "gram"
+    },
+    {
+        "name": "Poulet",
+        "lang": "de",
+        "amount": 100,
+        "unit": "gram"
+    },
+    ]
+    """
+    url = f"{BASE_URL}/api/kitchens/{DUMMY_KITCHEN_NAME}/recipes?indicators=true&transient=true"
+
+    req_ingredients = []
+
+    for idx, ing in enumerate(ingredients):
+        req_ingredients.append({
+            # Use new id for each request!!!
+            "id": int(time.time()) + idx,
+            "type": "conceptual-ingredients",
+            "names": [
+                {
+                    "language": ing['lang'],
+                    "value": ing['name']
+                }
+            ],
+            "amount": ing['amount'],
+            "unit": ing['unit'],
+        })
+
+    body = {
+        "recipe": {
+            "location": "Zürich Schweiz",
+            "recipe-portions": 1,
+            "ingredients": req_ingredients
+        }
+    }
+
+    response = requests.post(url, json=body, auth=AUTH)
+    if response.status_code not in [200, 201, 202]:
+        print(
+            f"ERROR: Failed creating recipe with status {response.status_code}: '{response.text}'")
+    else:
+        recipe = response.json()
+        # delete recipe as we don't use it
+        delete_recipe(recipe['recipe-id'])
+
+        return {
+            'co2-eq-in-g': recipe['recipe']['co2-value']
+        }
+
+
+if __name__ == '__main__':
+    # TODO Error handling (what happens if ingredient is not found)
+    ingredients = [
+        {
+            "name": "Chilli",
+            "lang": "de",
+            "amount": 100,
+            "unit": "gram"
+        },
+        {
+            "name": "Poulet",
+            "lang": "de",
+            "amount": 200,
+            "unit": "gram"
+        },
+    ]
+
+    scores = get_score(ingredients)
+
+    print(f"co2 value: {scores['co2-eq-in-g']}g CO₂equivalent")
